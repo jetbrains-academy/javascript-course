835d037bf7e04899c4c3c0fe62b0ebfd4abb190e
Konstantin Chaika <konstantin.chaika@jetbrains.com>
2024-04-01 15:01:14 +0400

    Workaround on expect-toBe error-message limitation

--- Variables/Example task with jest lib test/test/test.js
+++ Variables/Example task with jest lib test/test/test.js
@@ -1,5 +1,4 @@
 var rewire = require('rewire');
-const assert = require("assert");
 var task = rewire('../task')
 
 try {
@@ -12,14 +11,10 @@ catch (e) {
 }
 
 test('Returns 2 for input 1 1', () => {
-
-    // as of Jest 27, the toBe matcher does not support custom error messages
-    //expect(mySum(1, 1)).toBe(2, 'mySum(1,1) should returns 2')
-
-    //assert.equal(mySum(1, 1),2,'mySum(1,1) should returns 2')
-
-    const result = mySum(1, 1);
-    if (result !== 2) {
-        throw new Error(`Expected mySum(1, 1) returns 2, but received ${result}`);
+    try {
+        expect(mySum(1, 1)).toBe(2)
+    } catch (e){
+        e.message = e.message.replace(/.*\n/, 'mySum(1,1) should returns 2');
+        throw e
     }
 });
