6726b4dbea17f2669a910fcec13a4f250e11feb9
geravant <igor.gerasimov@jetbrains.com>
2024-10-13 22:30:20 +0200

    test (Data Types): enhanced analyzeDate tests in built_in_objects_array_and_date
    Implemented detailed tests for analyzeDate function to check string representation, year extraction, and human-readable format. Removed placeholder test.

--- Data Types/built_in_objects_array_and_date/test/test.js
+++ Data Types/built_in_objects_array_and_date/test/test.js
@@ -1,15 +1,66 @@
-/**
- * Imports task file in order to use functions declared in it
- * @param f file name or its relative path to task
- */
-function import_task_file(f) {
-    const path = require("path");
-    const fs = require("fs");
-    eval.apply(global, [fs.readFileSync(path.join(__dirname, "..",  f)).toString()]);
-}
+const analyzeDate = require('../task');
+const rewire = require("rewire");
+const utils = rewire('#utils/utils.js')
+customizeError = utils.__get__('customizeError')
 
-//todo: replace this with an actual test
-test('adds 1 + 2 to equal 3', () => {
-    import_task_file("task.js");
-    expect(sum(1, 2)).toBe(3);
+let consoleOutput = [];
+const originalConsoleLog = console.log;
+const expectedOutput = [
+    "",
+    "Year of the given date: 2023",
+    "Human-readable date string: Mon Oct 09 2023",
+
+];
+
+
+beforeAll(() => {
+    console.log = jest.fn((...args) => {
+        consoleOutput.push(args.join(' '));
+        originalConsoleLog(...args);
+    });
 });
+
+afterAll(() => {
+    console.log = originalConsoleLog;
+});
+
+beforeEach(() => {
+    consoleOutput = [];
+});
+
+test('Check if string representation is implemented correctly', () => {
+    analyzeDate(new Date('2023-10-09'));
+
+    try {
+        expect(consoleOutput[0]).toEqual("Current date and time: " + new Date().toString());
+    }
+    catch (e) {
+        customizeError(e, 'Check if string representation is implemented correctly', true)
+        throw e
+    }
+});
+
+test('Check if the year is implemented correctly', () => {
+    analyzeDate(new Date('2023-10-09'));
+
+    try {
+        expect(consoleOutput[1]).toEqual(expectedOutput[1]);
+    }
+    catch (e) {
+        customizeError(e, 'Check if the year is implemented correctly', true)
+        throw e
+    }
+});
+
+test('Check if human-readable format is implemented correctly', () => {
+    analyzeDate(new Date('2023-10-09'));
+
+    try {
+        expect(consoleOutput[2]).toEqual(expectedOutput[2]);
+    }
+    catch (e) {
+        customizeError(e, 'Check if human-readable format is implemented correctly', true)
+        throw e
+    }
+});
+
