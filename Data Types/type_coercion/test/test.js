cdb8349a7b310aa78dc2424cec4aea03a40ba994
geravant <igor.gerasimov@jetbrains.com>
2024-10-13 22:30:30 +0200

    feat (type coercion) updated tests: added detailed tests for type coercion functions with custom error handling

--- Data Types/type_coercion/test/test.js
+++ Data Types/type_coercion/test/test.js
@@ -1,15 +1,60 @@
-/**
- * Imports task file in order to use functions declared in it
- * @param f file name or its relative path to task
- */
-function import_task_file(f) {
-    const path = require("path");
-    const fs = require("fs");
-    eval.apply(global, [fs.readFileSync(path.join(__dirname, "..",  f)).toString()]);
-}
+const coerceTypes = require('../task');
+const rewire = require("rewire");
+const utils = rewire('#utils/utils.js')
+customizeError = utils.__get__('customizeError')
 
-//todo: replace this with an actual test
-test('adds 1 + 2 to equal 3', () => {
-    import_task_file("task.js");
-    expect(sum(1, 2)).toBe(3);
+let consoleOutput = [];
+const originalConsoleLog = console.log;
+
+
+beforeAll(() => {
+    console.log = jest.fn((...args) => {
+        consoleOutput.push(args.join(' '));
+        originalConsoleLog(...args);
+    });
 });
+
+afterAll(() => {
+    console.log = originalConsoleLog;
+});
+
+beforeEach(() => {
+    consoleOutput = [];
+});
+
+test('Check if string and number coercion is implemented correctly', () => {
+    actualResult = coerceTypes('5', 3);
+    expectedResult =8;
+    try {
+        expect(actualResult).toEqual(expectedResult);
+    }
+    catch (e) {
+        customizeError(e, 'Check if the coerceTypes is implemented correctly', true)
+        throw e
+    }
+});
+
+test('Check if string to number coercion  is implemented correctly', () => {
+    actualResult = coerceTypes('5', '3');
+    expectedResult = 8;
+    try {
+        expect(actualResult).toEqual(expectedResult);
+    }
+    catch (e) {
+        customizeError(e, 'Check if the coerceTypes is implemented correctly', true)
+        throw e
+    }
+});
+
+test('Check if bool coercion is implemented correctly', () => {
+    actualResult = coerceTypes(true, true);
+    expectedResult = 2;
+    try {
+        expect(actualResult).toEqual(expectedResult);
+    }
+    catch (e) {
+        customizeError(e, 'Check if coerceTypes is implemented correctly', true)
+        throw e
+    }
+});
+
